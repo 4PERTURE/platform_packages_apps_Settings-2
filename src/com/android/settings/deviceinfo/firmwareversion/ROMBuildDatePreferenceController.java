88d51b25163e483492aef7ae211e0ea07965c761
Chris Soyars <ctso@ctso.me>
2019-09-14 08:44:22 +0200

    Settings: Use simple way to show build date
     * partially cherry-pick from syberia-project/platform_packages_apps_Setting@f7824e7af98b8c4b0b558b4a5b64b25f331701e1
    
    Change-Id: Icdd55eec2d7248ad91e46190d34ec90886e97e04
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/ROMBuildDatePreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/ROMBuildDatePreferenceController.java
@@ -18,47 +18,31 @@ package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
 import android.os.SystemProperties;
-import android.text.format.DateFormat;
 import android.text.TextUtils;
 
-import com.android.settings.R;
-import com.android.settings.core.BasePreferenceController;
+import androidx.annotation.VisibleForTesting;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
 
 public class ROMBuildDatePreferenceController  extends BasePreferenceController {
 
-    private static final String TAG = "ROMBuildDatePreferenceController";
-    private static final String AOSIP_VERSION = "ro.aosip.version";
+    @VisibleForTesting
+    static final String BUILD_DATE_PROPERTY = "ro.aosip.build.date";
 
-    public ROMBuildDatePreferenceController(Context context, String key) {
-        super(context, key);
+    public ROMBuildDatePreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return AVAILABLE;
+        return !TextUtils.isEmpty(SystemProperties.get(BUILD_DATE_PROPERTY)) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
     public CharSequence getSummary() {
-        //Preparing the date string, taken from com.android.settingslib.DeviceInfoUtils
-        String aosipVersion = SystemProperties.get(AOSIP_VERSION);
-        String zipbuildate = aosipVersion.substring(aosipVersion.length() - 15);
-        if (!zipbuildate.isEmpty()) {
-            try {
-                SimpleDateFormat template = new SimpleDateFormat("yyyyMMdd");
-                Date buildDate = template.parse(zipbuildate);
-                String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");
-                return DateFormat.format(format, buildDate).toString();
-            } catch (ParseException e) {
-                return mContext.getString(R.string.unknown);
-            }
-        }
-        return mContext.getString(R.string.unknown);
+        return SystemProperties.get(BUILD_DATE_PROPERTY,
+                mContext.getString(R.string.device_info_default));
     }
 }
-
